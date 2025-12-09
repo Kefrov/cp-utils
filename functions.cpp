77bb04834964a53920307a6b43343966188c90dd
Fares Manai <fares.manai123@gmail.com>
2025-12-09 12:26:52 +0100

    add string hashing

--- functions.cpp
+++ functions.cpp
@@ -418,4 +418,17 @@ bool MillerRabin(u64 n) { // returns true if n is prime, else returns false.
     return true;
 }
 
+// To improve no-collision probability, duplicate this function (with a different modulo) then compare pair<ll,ll>
+ll compute_hash(string const& s) {
+    const int p = 31;
+    const int m = 1e9 + 9;
+    ll hash_value = 0;
+    ll p_pow = 1;
+    for (char c : s) {
+        hash_value = (hash_value + (c - 'a' + 1) * p_pow) % m;
+        p_pow = (p_pow * p) % m;
+    }
+    return hash_value;
+}
+
 int main() {}
