59e9d1a53d6b621618927dcd311e92ef33946d3c
Fares Manai <fares.manai123@gmail.com>
2026-04-06 19:38:17 +0100

    remove redundant gcd and lcm functions

--- functions.cpp
+++ functions.cpp
@@ -139,15 +139,6 @@ ll bin_inv(ll a, ll m = MOD) {
     return modpow(a, m - 2, m);
 }
 
-ll gcd(ll a, ll b) {
-    if (b == 0) return a;
-    return gcd(b, a % b);
-}
-
-ll lcm(ll a, ll b) {
-    return a / gcd(a, b) * b;
-}
-
 ll gcd_extended(ll a, ll b, ll& x, ll& y) { 
     if (b == 0) {
         x = 1;
